09c4eca8b0cf788f8a2dca47f7bddcbc0c5b539e
Maxime Hélias <maximehelias16@gmail.com>
2023-03-22 09:35:01 +0100

    CI Version

--- tests/Fixtures/Kernel.php
+++ tests/Fixtures/Kernel.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Odandb\MonitoringMetricsBundle\Tests\Fixtures;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\ORM\Configuration;
 use Odandb\MonitoringMetricsBundle\MonitoringMetricsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
@@ -19,6 +21,13 @@ class Kernel extends BaseKernel
             new DoctrineBundle(),
             new MonitoringMetricsBundle()
         ];
+    }
+
+    public function prepareContainer(ContainerBuilder $container): void
+    {
+        parent::prepareContainer($container);
+
+        $container->setParameter('enable_lazy_ghost_objects', method_exists(Configuration::class, 'isLazyGhostObjectEnabled'));
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
